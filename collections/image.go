084bd826b1959afcd0c72c90e836359c6d0ccab2
webus <webusnix@gmail.com>
2016-12-06 15:49:40 +0300

    added image collection and trash vendor file

---
+++ collections/image.go
@@ -0,0 +1,20 @@
+package collections
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+// ImageCollection struct
+type ImageCollection struct {
+	URL string
+	ETag string
+	LastModified string
+	FileID bson.ObjectId
+	ThumbnailFileID bson.ObjectId `bson:",omitempty"`
+	Image1FileID bson.ObjectId `bson:",omitempty"`
+	Image2FileID bson.ObjectId `bson:",omitempty"`
+	Image3FileID bson.ObjectId `bson:",omitempty"`
+	Hash string
+	Width int `bson:",omitempty"`
+	Height int `bson:",omitempty"`
+}
